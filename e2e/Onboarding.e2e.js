3611c6e09b0a8bf1f8f9d9420e72e54a9da406b8
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2020-09-04 16:17:25 +0000

    Add TODO.md

---
+++ e2e/Onboarding.e2e.js
@@ -0,0 +1,14 @@
+
+import { Create } from './usecases/Create'
+import { permissions } from './utils/fixtures'
+
+describe('Onboarding', () => {
+  beforeAll(async () => {
+    await device.launchApp({
+      delete: true,
+      permissions,
+    })
+  })
+
+  describe(Create, Create)
+})
