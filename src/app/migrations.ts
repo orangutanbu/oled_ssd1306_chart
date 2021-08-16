b23600883e8d650deb09bdb6a0b96f2daab23ee9
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2021-08-16 20:52:16 +0000

    Correction de bug

--- src/app/migrations.ts
+++ src/app/migrations.ts
@@ -6,4 +6,96 @@ import { ChainId } from 'src/constants/chains'
 import { ChainsState } from 'src/features/chains/chainsSlice'
 import { ensApi } from 'src/features/ens/api'
 import { ModalName } from 'src/features/telemetry/constants'
-import 
+import { TransactionState } from 'src/features/transactions/slice'
+import {
+  ChainIdToTxIdToDetails,
+  TransactionStatus,
+  TransactionType,
+} from 'src/features/transactions/types'
+import { Account, AccountType } from 'src/features/wallet/accounts/types'
+import { DEMO_ACCOUNT_ADDRESS } from 'src/features/wallet/accounts/useTestAccount'
+import { toSupportedChainId } from 'src/utils/chainId'
+
+export const migrations = {
+  0: (state: any) => {
+    const oldTransactionState = state?.transactions
+    const newTransactionState: any = {}
+
+    const chainIds = Object.keys(oldTransactionState?.byChainId ?? {})
+    for (const chainId of chainIds) {
+      const transactions = oldTransactionState.byChainId?.[chainId] ?? []
+      const txIds = Object.keys(transactions)
+      for (const txId of txIds) {
+        const txDetails = transactions[txId]
+        const address = txDetails.from
+        newTransactionState[address] ??= {}
+        newTransactionState[address][chainId] ??= {}
+        newTransactionState[address][chainId][txId] = { ...txDetails }
+      }
+    }
+
+    const oldNotificationState = state.notifications
+    const newNotificationState = { ...oldNotificationState, lastTxNotificationUpdate: {} }
+    const addresses = Object.keys(oldTransactionState?.lastTxHistoryUpdate || [])
+    for (const address of addresses) {
+      newNotificationState.lastTxNotificationUpdate[address] = {
+        [ChainId.Mainnet]: oldTransactionState.lastTxHistoryUpdate[address],
+      }
+    }
+
+    return { ...state, transactions: newTransactionState, notifications: newNotificationState }
+  },
+
+  1: (state: any) => {
+    const newState = { ...state }
+    delete newState.walletConnect?.modalState
+    return newState
+  },
+
+  2: (state: any) => {
+    const newState = { ...state }
+    const oldFollowingAddresses = state?.favorites?.followedAddresses
+    if (oldFollowingAddresses) newState.favorites.watchedAddresses = oldFollowingAddresses
+    delete newState?.favorites?.followedAddresses
+    return newState
+  },
+
+  3: (state: any) => {
+    const newState = { ...state }
+    newState.searchHistory = { results: [] }
+    return newState
+  },
+
+  4: (state: any) => {
+    const newState = { ...state }
+    const accounts = newState?.wallet?.accounts ?? {}
+    let derivationIndex = 0
+    for (const account of Object.keys(accounts)) {
+      newState.wallet.accounts[account].timeImportedMs = dayjs().valueOf()
+      if (newState.wallet.accounts[account].type === 'native') {
+        newState.wallet.accounts[account].derivationIndex = derivationIndex
+        derivationIndex += 1
+      }
+    }
+    return newState
+  },
+
+  5: (state: any) => {
+    const newState = { ...state }
+    newState.modals = {
+      [ModalName.WalletConnectScan]: {
+        isOpen: false,
+        initialState: 0,
+      },
+      [ModalName.Swap]: {
+        isOpen: false,
+        initialState: undefined,
+      },
+      [ModalName.Send]: {
+        isOpen: false,
+        initialState: undefined,
+      },
+    }
+
+    delete newState?.balances
+    r
