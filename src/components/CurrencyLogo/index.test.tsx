20d2d343b96720d6f91d97aec4e4819df3eb70fe
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-05-02 16:13:39 +0000

    typo--

--- src/components/CurrencyLogo/index.test.tsx
+++ src/components/CurrencyLogo/index.test.tsx
@@ -3,4 +3,14 @@ import { CurrencyLogo } from 'src/components/CurrencyLogo'
 import { arbitrumDaiCurrencyInfo, uniCurrencyInfo } from 'src/test/fixtures'
 import { render } from 'src/test/test-utils'
 
-jest
+jest.mock('src/assets', () => 'ethereum-logo.png')
+
+it('renders a currency logo without network logo', () => {
+  const tree = render(<CurrencyLogo currencyInfo={uniCurrencyInfo} size={20} />)
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders a currency logo with network logo', () => {
+  const tree = render(<CurrencyLogo currencyInfo={arbitrumDaiCurrencyInfo} size={20} />)
+  expect(tree).toMatchSnapshot()
+})
