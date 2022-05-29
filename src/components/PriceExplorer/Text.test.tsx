3ed7eb9c47c4b6a58543cab4c69d0e889a62ef5c
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-05-29 14:32:03 +0000

    ALSA: hda: Cirrus: Fix coefficient index for beep configuration

--- src/components/PriceExplorer/Text.test.tsx
+++ src/components/PriceExplorer/Text.test.tsx
@@ -47,4 +47,68 @@ describe(PriceText, () => {
     const animatedText = await tree.findByTestId('price-text')
     const wholePart = await within(animatedText).findByTestId('wholePart')
     const decimalPart = await within(animatedText).findByTestId('decimalPart')
-    e
+    expect(wholePart.props.animatedProps.text).toBe(`$${Amounts.sm.value}`)
+    expect(decimalPart.props.animatedProps.text).toBe(`.00`)
+  })
+})
+
+describe(RelativeChangeText, () => {
+  it('renders without error', () => {
+    mockedUseLineChart.mockReturnValue({
+      isActive: { value: false },
+    })
+
+    const tree = render(
+      <RelativeChangeText loading={false} spotRelativeChange={{ value: Amounts.md.value }} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders loading state', () => {
+    mockedUseLineChart.mockReturnValue({
+      isActive: { value: false },
+    })
+
+    const tree = render(
+      <RelativeChangeText loading={true} spotRelativeChange={{ value: Amounts.md.value }} />
+    )
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('shows active relative change when scrubbing', async () => {
+    mockedUseLineChart.mockReturnValue({
+      isActive: { value: true },
+      data: [{ value: 10 }, { value: 9 }],
+      currentIndex: { value: 1 },
+    })
+
+    const tree = render(<RelativeChangeText loading={false} />)
+
+    const text = await tree.findByTestId('relative-change-text')
+    expect(text.props.value).toBe(`10.00%`)
+  })
+})
+
+describe(DatetimeText, () => {
+  it('renders without error', () => {
+    mockedUseLineChartDatetime.mockReturnValue({
+      value: { value: '123' },
+      formatted: { value: 'Thursday, November 1st, 2023' },
+    })
+    const tree = render(<DatetimeText loading={false} />)
+
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('renders loading state', () => {
+    mockedUseLineChartDatetime.mockReturnValue({
+      value: { value: '123' },
+      formatted: { value: 'Thursday, November 1st, 2023' },
+    })
+    const tree = render(<DatetimeText loading={true} />)
+
+    expect(tree).toMatchSnapshot()
+  })
+})
---
+++ src/components/PriceExplorer/Text.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { SharedValue, useAnimatedStyle, useDerivedValue } from 'react-native-reanimated'
+import { useLineChartDatetime } from 'react-native-wagmi-charts'
+import { useAppTheme } from 'src/app/hooks'
+import { Flex } from 'src/components/layout'
+import { Text } from 'src/components/Text'
+import { AnimatedText } from 'src/components/text/AnimatedText'
+import { AnimatedDecimalNumber } from './AnimatedDecimalNumber'
+import { useLineChartPrice, useLineChartRelativeChange } from './usePrice'
+
+export function PriceText({ loading }: { loading: boolean }): JSX.Element {
+  const price = useLineChartPrice()
+
+  if (loading) {
+    return <Text loading loadingPlaceholderText="$10,000" variant="headlineLarge" />
+  }
+
+  return <AnimatedDecimalNumber number={price} testID="price-text" variant="headlineLarge" />
+}
+
+export function RelativeChangeText({
+  loading,
+  spotRelativeChange,
+}: {
+  loading: boolean
+  spotRelativeChange?: SharedValue<number>
+}): JSX.Element {
+  const theme = useAppTheme()
+
+  const relativeChange = useLineChartRelativeChange({ spotRelativeChange })
+  const icon = useDerivedValue(() => (relativeChange.value.value > 0 ? '↗' : '↘'))
+  const styles = useAnimatedStyle(() => ({
+    color:
+      rel
