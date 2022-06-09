70525945b9dc84e2c848dc322ce75b90bed2516c
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-06-09 16:57:01 +0000

    Messaging

---
+++ src/components/TokenDetails/TokenDetailsActionButton.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { useAppTheme } from 'src/app/hooks'
+import { ButtonEmphasis, ButtonSize } from 'src/components/buttons/Button'
+import { TouchableArea } from 'src/components/buttons/TouchableArea'
+import { getButtonProperties } from 'src/components/buttons/utils'
+import { Flex } from 'src/components/layout'
+import { TracePressEv
