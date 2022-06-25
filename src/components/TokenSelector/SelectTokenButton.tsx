64ca6863b6953e6e45e6fa2c16ecdad84458f1e9
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-06-25 10:01:11 +0000

    fixes #117

--- src/components/TokenSelector/SelectTokenButton.tsx
+++ src/components/TokenSelector/SelectTokenButton.tsx
@@ -28,4 +28,29 @@ export function SelectTokenButton({
   return (
     <TouchableArea
       hapticFeedback
-     
+      bg={selectedCurrencyInfo ? 'background3' : 'magentaVibrant'}
+      borderRadius="roundedFull"
+      name={ElementName.TokenSelectorToggle}
+      testID={`currency-selector-toggle-${showNonZeroBalancesOnly ? 'in' : 'out'}`}
+      onPress={onPress}>
+      {selectedCurrencyInfo ? (
+        <Flex centered row flexDirection="row" gap="spacing4" p="spacing4">
+          <CurrencyLogo currencyInfo={selectedCurrencyInfo} size={iconSizes.icon28} />
+          <Text color="textPrimary" pl="spacing4" variant="buttonLabelLarge">
+            {selectedCurrencyInfo.currency.symbol}
+          </Text>
+          <Chevron color={theme.colors.textTertiary} direction="e" />
+        </Flex>
+      ) : (
+        <Flex centered row py="spacing4">
+          <Flex centered row gap="spacing4" pl="spacing12" pr="spacing8" py="spacing2">
+            <Text color="textOnBrightPrimary" variant="buttonLabelLarge">
+              {t('Choose a token')}
+            </Text>
+            <Chevron color={theme.colors.textOnBrightPrimary} direction="e" />
+          </Flex>
+        </Flex>
+      )}
+    </TouchableArea>
+  )
+}
