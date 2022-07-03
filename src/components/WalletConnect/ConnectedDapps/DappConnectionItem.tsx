d1c1769c4e66a71d31e16b935ebf32106730acc5
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-07-03 08:51:37 +0000

    Updated the xeus feedstock.

--- src/components/WalletConnect/ConnectedDapps/DappConnectionItem.tsx
+++ src/components/WalletConnect/ConnectedDapps/DappConnectionItem.tsx
@@ -36,4 +36,97 @@ export function DappConnectionItem({
       justifyContent="space-between"
       mb="spacing12"
       px="spacing12"
-   
+      py="spacing16"
+      width="48%">
+      <TouchableArea
+        flex={1}
+        name={ElementName.WCOpenDapp}
+        onPress={(): Promise<void> => openUri(dapp.url)}>
+        <Flex centered grow gap="spacing8">
+          <Flex fill>
+            <DappHeaderIcon dapp={dapp} showChain={false} />
+          </Flex>
+          <Text numberOfLines={2} textAlign="center" variant="buttonLabelMedium">
+            {dapp.name || dapp.url}
+          </Text>
+          <Text
+            color="accentActive"
+            numberOfLines={1}
+            textAlign="center"
+            variant="buttonLabelMicro">
+            {dapp.url}
+          </Text>
+        </Flex>
+      </TouchableArea>
+      {session.version === '1' ? (
+        <ChangeNetworkButton session={session} onPressChangeNetwork={onPressChangeNetwork} />
+      ) : (
+        <Flex centered>
+          <Box
+            flexDirection="row"
+            height={theme.iconSizes.icon28}
+            width={
+              theme.iconSizes.icon28 +
+              (session.chains.length - 1) * theme.iconSizes.icon28 * (2 / 3)
+            }>
+            {session.chains.map((chainId, index) => (
+              <Box
+                key={chainId}
+                left={index * theme.iconSizes.icon28 * (2 / 3)}
+                position="absolute">
+                <NetworkLogo chainId={chainId} size={theme.iconSizes.icon28} />
+              </Box>
+            ))}
+          </Box>
+        </Flex>
+      )}
+    </Flex>
+  )
+}
+
+function ChangeNetworkButton({
+  session,
+  onPressChangeNetwork,
+}: {
+  session: WalletConnectSessionV1
+  onPressChangeNetwork: (session: WalletConnectSessionV1) => void
+}): JSX.Element {
+  const theme = useAppTheme()
+  const { t } = useTranslation()
+
+  // Only WC v1.0 connections have a current chain_id
+  const supportedChainId = toSupportedChainId(session.dapp.chain_id)
+  const isDarkMode = useColorScheme() === 'dark'
+
+  return (
+    <TouchableArea
+      name={ElementName.WCDappSwitchNetwork}
+      onPress={(): void => onPressChangeNetwork(session)}>
+      <Flex
+        row
+        shrink
+        backgroundColor={isDarkMode ? 'background3' : 'background3'}
+        borderRadius="roundedFull"
+        gap="none"
+        justifyContent="space-between"
+        p="spacing8">
+        {supportedChainId ? (
+          <Flex fill row shrink gap="spacing8">
+            <NetworkLogo chainId={supportedChainId} />
+            <Flex shrink>
+              <Text
+                color="textSecondary"
+                numberOfLines={1}
+                textAlign="center"
+                variant="buttonLabelSmall">
+                {CHAIN_INFO[supportedChainId].label}
+              </Text>
+            </Flex>
+          </Flex>
+        ) : (
+          <Text color="textSecondary" textAlign="center" variant="buttonLabelSmall">
+            {t('Unsupported chain')}
+          </Text>
+        )}
+        <Chevron
+          color={theme.colors.textTertiar
