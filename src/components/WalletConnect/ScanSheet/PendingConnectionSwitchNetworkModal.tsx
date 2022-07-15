fb0fea3e93bc5e7dd63764cf015de73cfce7ed65
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-07-15 11:31:13 +0000

    I18n: Update translation sq (98%).

--- src/components/WalletConnect/ScanSheet/PendingConnectionSwitchNetworkModal.tsx
+++ src/components/WalletConnect/ScanSheet/PendingConnectionSwitchNetworkModal.tsx
@@ -14,4 +14,63 @@ import { iconSizes } from 'src/styles/sizing'
 
 type Props = {
   selectedChainId: ChainId
-  onPr
+  onPressChain: (chainId: ChainId) => void
+  onClose: () => void
+}
+
+export const PendingConnectionSwitchNetworkModal = ({
+  selectedChainId,
+  onPressChain,
+  onClose,
+}: Props): JSX.Element => {
+  const activeChains = useActiveChainIds()
+  const theme = useAppTheme()
+  const { t } = useTranslation()
+
+  const options = useMemo(
+    () =>
+      activeChains.map((chainId) => {
+        const info = CHAIN_INFO[chainId]
+        return {
+          key: `${ElementName.NetworkButton}-${chainId}`,
+          onPress: () => onPressChain(chainId),
+          render: () => (
+            <>
+              <Separator />
+              <Flex
+                row
+                alignItems="center"
+                justifyContent="space-between"
+                px="spacing24"
+                py="spacing16">
+                <NetworkLogo chainId={chainId} size={iconSizes.icon24} />
+                <Text color="textPrimary" variant="bodyLarge">
+                  {info.label}
+                </Text>
+                <Box height={24} width={24}>
+                  {chainId === selectedChainId && (
+                    <Check color={theme.colors.accentActive} height={24} width={24} />
+                  )}
+                </Box>
+              </Flex>
+            </>
+          ),
+        }
+      }),
+    [activeChains, selectedChainId, onPressChain, theme.colors.accentActive]
+  )
+
+  return (
+    <ActionSheetModal
+      header={
+        <Flex centered gap="spacing4" py="spacing16">
+          <Text variant="buttonLabelMedium">{t('Switch Network')}</Text>
+        </Flex>
+      }
+      isVisible={true}
+      name={ModalName.NetworkSelector}
+      options={options}
+      onClose={onClose}
+    />
+  )
+}
