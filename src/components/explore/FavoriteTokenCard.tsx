55125d4556f708e1878758e67220f83cfb2c16b6
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-08-06 15:18:10 +0000

    Update CodeBook.MD

--- src/components/explore/FavoriteTokenCard.tsx
+++ src/components/explore/FavoriteTokenCard.tsx
@@ -20,4 +20,86 @@ import { PollingInterval } from 'src/constants/misc'
 import { isNonPollingRequestInFlight } from 'src/data/utils'
 import { useFavoriteTokenCardQuery } from 'src/data/__generated__/types-and-hooks'
 import { AssetType } from 'src/entities/assets'
-i
+import { currencyIdToContractInput } from 'src/features/dataApi/utils'
+import { removeFavoriteToken } from 'src/features/favorites/slice'
+import { openModal } from 'src/features/modals/modalSlice'
+import { sendAnalyticsEvent } from 'src/features/telemetry'
+import { ElementName, ModalName, SectionName } from 'src/features/telemetry/constants'
+import {
+  CurrencyField,
+  TransactionState,
+} from 'src/features/transactions/transactionState/transactionState'
+import { fromGraphQLChain } from 'src/utils/chainId'
+import { formatUSDPrice } from 'src/utils/format'
+import { usePollOnFocusOnly } from 'src/utils/hooks'
+
+export const FAVORITE_TOKEN_CARD_LOADER_HEIGHT = 102
+
+type FavoriteTokenCardProps = {
+  currencyId: string
+  isEditing?: boolean
+  setIsEditing: (update: boolean) => void
+} & ViewProps
+
+function FavoriteTokenCard({
+  currencyId,
+  isEditing,
+  setIsEditing,
+  ...rest
+}: FavoriteTokenCardProps): JSX.Element {
+  const theme = useAppTheme()
+  const { t } = useTranslation()
+  const dispatch = useAppDispatch()
+  const tokenDetailsNavigation = useTokenDetailsNavigation()
+
+  const { data, networkStatus, startPolling, stopPolling } = useFavoriteTokenCardQuery({
+    variables: currencyIdToContractInput(currencyId),
+    // Rely on cache for fast favoriting UX, and poll for updates.
+    fetchPolicy: 'cache-first',
+    returnPartialData: true,
+  })
+
+  usePollOnFocusOnly(startPolling, stopPolling, PollingInterval.Fast)
+
+  const token = data?.token
+
+  // Mirror behavior in top tokens list, use first chain the token is on for the symbol
+  const chainId = fromGraphQLChain(token?.chain)
+
+  const usdPrice = token?.project?.markets?.[0]?.price?.value
+  const pricePercentChange = token?.project?.markets?.[0]?.pricePercentChange24h?.value
+
+  const onRemove = useCallback(() => {
+    if (currencyId) {
+      dispatch(removeFavoriteToken({ currencyId }))
+    }
+  }, [currencyId, dispatch])
+
+  const navigateToSwapSell = useCallback(() => {
+    if (!token?.address || !chainId) return
+
+    const swapFormState: TransactionState = {
+      exactCurrencyField: CurrencyField.INPUT,
+      exactAmountToken: '0',
+      [CurrencyField.INPUT]: {
+        address: token.address,
+        chainId,
+        type: AssetType.Currency,
+      },
+      [CurrencyField.OUTPUT]: null,
+    }
+    dispatch(openModal({ name: ModalName.Swap, initialState: swapFormState }))
+  }, [chainId, dispatch, token?.address])
+
+  const menuActions = useMemo(() => {
+    return [
+      { title: t('Remove favorite'), systemIcon: 'heart.fill' },
+      { title: t('Edit favorites'), systemIcon: 'square.and.pencil' },
+      { title: t('Swap'), systemIcon: 'arrow.2.squarepath' },
+    ]
+  }, [t])
+
+  const onPress = (): void => {
+    if (isEditing || !currencyId) return
+    tokenDetailsNavigation.preload(currencyId)
+    tokenDetailsNavigation.navigate(currencyId, token?.name ??
