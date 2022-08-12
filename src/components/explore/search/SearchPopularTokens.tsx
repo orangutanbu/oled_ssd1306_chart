0c8fab39cdcabc19372f9b69275964bec7d3a514
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-08-12 08:28:02 +0000

    code cleaning

--- src/components/explore/search/SearchPopularTokens.tsx
+++ src/components/explore/search/SearchPopularTokens.tsx
@@ -26,4 +26,69 @@ export function SearchPopularTokens(): JSX.Element {
     // special case to replace weth with eth because the backend does not return eth data
     // eth will be defined only if all the required data is available
     // when eth data is not fully available, we do not replace weth with eth
-    const eth = data?.
+    const eth = data?.eth && data?.eth.length > 0 && data?.eth?.[0]?.project ? data.eth[0] : null
+    const weth = WRAPPED_NATIVE_CURRENCY[ChainId.Mainnet]
+
+    return data.topTokens
+      .map((token) => {
+        if (!token) return
+
+        const isWeth =
+          areAddressesEqual(token.address, weth.address) && token?.chain === Chain.Ethereum
+
+        // manually replace weth with eth given backend only returns eth data as a proxy for eth
+        if (isWeth && eth) {
+          return gqlTokenToTokenSearchResult(eth)
+        }
+
+        return gqlTokenToTokenSearchResult(token)
+      })
+      .filter((t): t is TokenSearchResult => Boolean(t))
+  }, [data])
+
+  if (loading) {
+    return (
+      <Inset all="spacing8">
+        <Loader.Token repeat={3} />
+      </Inset>
+    )
+  }
+
+  return (
+    <FlatList
+      data={popularTokens}
+      keyExtractor={tokenKey}
+      listKey="tokens"
+      renderItem={renderTokenItem}
+    />
+  )
+}
+
+function gqlTokenToTokenSearchResult(
+  token: NullUndefined<NonNullable<NonNullable<SearchPopularTokensQuery['topTokens']>[0]>>
+): TokenSearchResult | null {
+  if (!token || !token.project) return null
+
+  const { chain, address, symbol, name, project } = token
+  const chainId = fromGraphQLChain(chain)
+  if (!chainId || !symbol || !name) return null
+
+  return {
+    type: SearchResultType.Token,
+    chainId,
+    address,
+    name,
+    symbol,
+    logoUrl: project?.logoUrl,
+  } as TokenSearchResult
+}
+
+const renderTokenItem = ({ item }: ListRenderItemInfo<TokenSearchResult>): JSX.Element => (
+  <SearchTokenItem token={item} />
+)
+
+const tokenKey = (token: TokenSearchResult): string => {
+  return token.address
+    ? buildCurrencyId(token.chainId, token.address)
+    : buildNativeCurrencyId(token.chainId)
+}
