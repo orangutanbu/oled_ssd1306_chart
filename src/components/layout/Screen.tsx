b9b5b62ee96650fc06abb96b1df03c8d3665b3ad
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-09-29 20:48:49 +0000

    Setting version to 1.0.1

--- src/components/layout/Screen.tsx
+++ src/components/layout/Screen.tsx
@@ -14,4 +14,53 @@ type ScreenProps = BackgroundColorShorthandProps<Theme> &
   Omit<NativeSafeAreaViewProps, 'mode'> &
   BoxProps
 
-function S
+function SafeAreaWithInsets({ children, edges, ...rest }: ScreenProps): JSX.Element {
+  // Safe area insets are wrong (0 when they shouldn't be) when using the <SafeAreaView>
+  // component from react-native-safe-area-context, because when the initial screen is
+  // outside the viewport (as is the case with a screen slide-in animation on navigation)
+  // the safe area insets are calculated based on the initial screen, not the final screen.
+  // This is a known issue with react-native-safe-area-context, and the solution is to use
+  // the useSafeAreaInsets hook instead. See:
+  // https://github.com/th3rdwave/react-native-safe-area-context/issues/114
+  const insets = useSafeAreaInsets()
+
+  const safeAreaStyles = useMemo(() => {
+    const style: { [key: string]: number } = {}
+    // Default to all edges, use empty array for no edges.
+    if (!edges) {
+      return {
+        paddingTop: insets.top,
+        paddingBottom: insets.bottom,
+        paddingLeft: insets.left,
+        paddingRight: insets.right,
+      }
+    }
+    if (edges?.includes('top')) {
+      style.paddingTop = insets.top
+    }
+    if (edges?.includes('bottom')) {
+      style.paddingBottom = insets.bottom
+    }
+    if (edges?.includes('left')) {
+      style.paddingLeft = insets.left
+    }
+    if (edges?.includes('right')) {
+      style.paddingRight = insets.right
+    }
+    return style
+  }, [edges, insets])
+
+  return (
+    <SafeAreaBox style={safeAreaStyles} {...rest}>
+      {children}
+    </SafeAreaBox>
+  )
+}
+
+export function Screen({ bg = 'background0', children, ...rest }: ScreenProps): JSX.Element {
+  return (
+    <SafeAreaWithInsets bg={bg} flex={1} {...rest}>
+      {children}
+    </SafeAreaWithInsets>
+  )
+}
---
+++ src/components/layout/screens/HeaderScrollScreen.tsx
@@ -0,0 +1,3 @@
+import { useScrollToTop } from '@react-navigation/native'
+import React, { PropsWithChildren, useRef } from 'react'
+import { FlatList } from 'react-native-gesture-han
