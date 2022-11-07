514468c1eb905333d91078152109384709296b60
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-11-07 19:31:34 +0000

    * MAINTAINERS: Update my email address.

--- src/components/mnemonic/MnemonicTest.tsx
+++ src/components/mnemonic/MnemonicTest.tsx
@@ -9,4 +9,29 @@ interface NativeMnemonicTestProps {
   onTestComplete: () => void
 }
 
-const NativeMnemonicTest = requireNati
+const NativeMnemonicTest = requireNativeComponent<NativeMnemonicTestProps>('MnemonicTest')
+
+type MnemonicTestProps = ViewProps & {
+  mnemonicId: Address
+  onTestComplete: () => void
+}
+
+const mnemonicTestStyle = (shouldShowSmallVersion: boolean): StyleProp<BoxProps> => {
+  return {
+    // This is the min height needed for native component to function correctly.
+    // We handle padding separately wherever the component is placed.
+    height: shouldShowSmallVersion ? 380 : 450,
+  }
+}
+
+export function MnemonicTest(props: MnemonicTestProps): JSX.Element {
+  const shouldShowSmallText = dimensions.fullHeight < 700
+
+  return (
+    <NativeMnemonicTest
+      shouldShowSmallText={shouldShowSmallText}
+      style={mnemonicTestStyle(shouldShowSmallText)}
+      {...props}
+    />
+  )
+}
---
+++ src/components/modals/ActionSheetModal.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from 'react'
+import { useTranslation } from 'react-i18next'
+import { ScrollView } from 'react-native-gesture-handler'
+import { TouchableArea } from 'src/components/buttons/TouchableArea'
+import { Flex } from 'src/components/layout'
+import { BottomSheetDetachedModal } from 'src/components/modals/BottomSheetModal'
+import { Text } from 'src/components/Text'
+import { ModalName } from
