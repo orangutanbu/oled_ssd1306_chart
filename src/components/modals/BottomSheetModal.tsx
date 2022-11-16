e6e25fa043bf3d36054cd1965ee0257fb6e637c7
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-11-16 21:11:47 +0000

    doc edits

--- src/components/modals/BottomSheetModal.tsx
+++ src/components/modals/BottomSheetModal.tsx
@@ -13,4 +13,109 @@ import { useAppTheme } from 'src/app/hooks'
 import { HandleBar } from 'src/components/modals/HandleBar'
 import { Trace } from 'src/components/telemetry/Trace'
 import { ModalName } from 'src/features/telemetry/constants'
-import { Te
+import { TelemetryTraceProps } from 'src/features/telemetry/types'
+import { dimensions, spacing } from 'src/styles/sizing'
+import { theme as FixedTheme } from 'src/styles/theme'
+
+type Props = PropsWithChildren<{
+  disableSwipe?: boolean
+  hideHandlebar?: boolean
+  name: ModalName
+  onClose?: () => void
+  snapPoints?: Array<string | number>
+  stackBehavior?: ComponentProps<typeof BaseModal>['stackBehavior']
+  fullScreen?: boolean
+  backgroundColor?: string
+  blurredBackground?: boolean
+  isDismissible?: boolean
+  renderBehindInset?: boolean
+  hideKeyboardOnDismiss?: boolean
+}> &
+  TelemetryTraceProps
+
+const APPEARS_ON_INDEX = 0
+const DISAPPEARS_ON_INDEX = -1
+
+const Backdrop = (props: BottomSheetBackdropProps): JSX.Element => {
+  return (
+    <BottomSheetBackdrop
+      {...props}
+      appearsOnIndex={APPEARS_ON_INDEX}
+      disappearsOnIndex={DISAPPEARS_ON_INDEX}
+      opacity={0.4}
+    />
+  )
+}
+
+const CONTENT_HEIGHT_SNAP_POINTS = ['CONTENT_HEIGHT']
+const FULL_HEIGHT = 0.91
+
+export function BottomSheetModal({
+  children,
+  name,
+  properties,
+  onClose,
+  snapPoints = CONTENT_HEIGHT_SNAP_POINTS,
+  stackBehavior = 'push',
+  fullScreen,
+  hideHandlebar,
+  backgroundColor,
+  blurredBackground = false,
+  isDismissible = true,
+  renderBehindInset = false,
+  hideKeyboardOnDismiss = false,
+}: Props): JSX.Element {
+  const insets = useSafeAreaInsets()
+  const modalRef = useRef<BaseModal>(null)
+  const { animatedHandleHeight, animatedSnapPoints, animatedContentHeight, handleContentLayout } =
+    useBottomSheetDynamicSnapPoints(snapPoints)
+  const theme = useAppTheme()
+  const isDarkMode = useColorScheme() === 'dark'
+
+  const backgroundColorValue = blurredBackground
+    ? theme.colors.none
+    : backgroundColor ?? theme.colors.background1
+
+  const renderBackdrop = useCallback(
+    (props) => (
+      <BottomSheetBackdrop
+        {...props}
+        appearsOnIndex={APPEARS_ON_INDEX}
+        disappearsOnIndex={DISAPPEARS_ON_INDEX}
+        opacity={blurredBackground ? 0.2 : 0.4}
+        pressBehavior={isDismissible ? 'close' : 'none'}
+      />
+    ),
+    [blurredBackground, isDismissible]
+  )
+
+  const renderHandleBar = useCallback(
+    (props) => {
+      // This adds an extra gap of unwanted space
+      if (renderBehindInset && hideHandlebar) {
+        return null
+      }
+      return (
+        <HandleBar
+          {...props}
+          backgroundColor={backgroundColorValue}
+          containerFlexStyles={{
+            marginBottom: FixedTheme.spacing.spacing12,
+            marginTop: FixedTheme.spacing.spacing16,
+          }}
+          hidden={hideHandlebar}
+        />
+      )
+    },
+    [backgroundColorValue, hideHandlebar, renderBehindInset]
+  )
+
+  useEffect(() => {
+    modalRef.current?.present()
+  }, [modalRef])
+
+  const fullScreenContentHeight = (renderBehindInset ? 1 : FULL_HEIGHT) * dimensions.fullHeight
+
+  const renderBlurredBg = useCallback(
+    () => (
+   
