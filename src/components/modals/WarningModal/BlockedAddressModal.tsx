99810034a1fd126b1afacc1c530603eab9eed3e9
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-11-27 06:29:12 +0000

    added some files

---
+++ src/components/modals/WarningModal/BlockedAddressModal.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+import { WarningSeverity } from 'src/components/modals/WarningModal/types'
+import WarningModal from 'src/components/modals/WarningModal/WarningModal'
+import { ModalName } from 'src/features/telemetry/constants'
+
+export function BlockedAddressModal({ onClose }: { onClose: () => void }): JSX.Element {
+  const { t } = useTranslation()
+
+  return (
+    <WarningModal
+      caption={t(`This address is blocked on Uniswap Wallet because it is associated with one or more blocked acti
