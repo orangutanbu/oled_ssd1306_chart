108ddf5d2b90b5b8dfc54ecae8d4f78e0344bb22
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2022-11-30 11:37:07 +0000

    Release 0.0.2.

--- src/components/modals/WarningModal/BlockedAddressModal.tsx
+++ src/components/modals/WarningModal/BlockedAddressModal.tsx
@@ -9,4 +9,14 @@ export function BlockedAddressModal({ onClose }: { onClose: () => void }): JSX.E
 
   return (
     <WarningModal
-      caption={t(`This address is blocked on Uniswap Wallet because it is associated with one or more blocked acti
+      caption={t(`This address is blocked on Uniswap Wallet because it is associated with one or more blocked activities. 
+
+If you believe this is an error, please email compliance@uniswap.org.`)}
+      closeText={t('I understand')}
+      modalName={ModalName.BlockedAddress}
+      severity={WarningSeverity.None}
+      title={t('Blocked address')}
+      onClose={onClose}
+    />
+  )
+}
