06a40a11af1d09b2e681a4a7c1834426fbee71f9
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-03 11:01:09 +0000

    USB: ftdi_sio: fixed handling of unsupported CSIZE setting

--- src/config.ts
+++ src/config.ts
@@ -35,4 +35,48 @@ export interface Config {
   moonpayApiUrl: string
   moonpayWidgetApiUrl: string
   uniswapApiBaseUrl: string
-  unis
+  uniswapApiKey: string
+  uniswapAppUrl: string
+  infuraProjectId: string
+  onesignalAppId: string
+  sentryDsn: string
+  statSigApiKey: string
+  statSigProxyUrl: string
+  version: string
+  walletConnectProjectId: string
+}
+
+const _config: Config = {
+  activeChains: chainListToStateMap(parseActiveChains(ACTIVE_CHAINS)),
+  amplitudeApiUrl: AMPLITUDE_API_URL,
+  amplitudeExperimentsDeploymentKey:
+    process.env.AMPLITUDE_EXPERIMENTS_DEPLOYMENT_KEY || AMPLITUDE_EXPERIMENTS_DEPLOYMENT_KEY,
+  alchemyApiKey: process.env.ALCHEMY_API_KEY || ALCHEMY_API_KEY,
+  debug: parseBoolean(DEBUG),
+  demoSeedPhrase: process.env.DEMO_SEED_PHRASE || DEMO_SEED_PHRASE,
+  moonpayApiKey: process.env.MOONPAY_API_KEY || MOONPAY_API_KEY,
+  moonpayApiUrl: process.env.MOONPAY_API_URL || MOONPAY_API_URL,
+  moonpayWidgetApiUrl: process.env.MOONPAY_WIDGET_API_URL || MOONPAY_WIDGET_API_URL,
+  uniswapApiBaseUrl: process.env.UNISWAP_API_BASE_URL || UNISWAP_API_BASE_URL,
+  uniswapApiKey: process.env.UNISWAP_API_KEY || UNISWAP_API_KEY,
+  uniswapAppUrl: UNISWAP_APP_URL,
+  infuraProjectId: process.env.INFURA_PROJECT_ID || INFURA_PROJECT_ID,
+  onesignalAppId: process.env.ONESIGNAL_APP_ID || ONESIGNAL_APP_ID,
+  sentryDsn: process.env.SENTRY_DSN || SENTRY_DSN,
+  statSigApiKey: process.env.STATSIG_API_KEY || STATSIG_API_KEY,
+  statSigProxyUrl: process.env.STATSIG_PROXY_URL || STATSIG_PROXY_URL,
+  version: VERSION,
+  walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID || WALLETCONNECT_PROJECT_ID,
+}
+
+function parseBoolean(value: string): boolean {
+  return value?.toLowerCase() === 'true'
+}
+
+export const config = Object.freeze(_config)
+
+if (config.debug) {
+  // Cannot use logger here, causes error from circular dep
+  // eslint-disable-next-line no-console
+  console.debug('Using app config:', config)
+}
