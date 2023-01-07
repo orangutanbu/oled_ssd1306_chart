cbda470b87c81d821e7b91f722c428f357300ae1
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-07 16:00:35 +0000

    Create README.rst

--- src/features/contracts/ContractManager.ts
+++ src/features/contracts/ContractManager.ts
@@ -14,4 +14,52 @@ export class ContractManager {
     provider: providers.Provider,
     ABI: ContractInterface
   ): Contract {
-    if (isNativeCurrencyAddress(chainId, address) || !getValidAddress(address
+    if (isNativeCurrencyAddress(chainId, address) || !getValidAddress(address, true)) {
+      throw Error(`Invalid address for contract: ${address}`)
+    }
+    this._contracts[chainId] ??= {}
+    if (this._contracts[chainId]![address]) {
+      throw new Error(`Contract already exists for: ${chainId} ${address}`)
+    } else {
+      logger.debug(
+        'ContractManager',
+        'createContract',
+        `Creating a new contract for: ${chainId} ${address}`
+      )
+      const contract = new Contract(address, ABI, provider)
+      this._contracts[chainId]![address] = contract
+      return contract
+    }
+  }
+
+  removeContract(chainId: ChainId, address: Address): void {
+    if (!this._contracts[chainId]?.[address]) {
+      logger.warn(
+        'ContractManager',
+        'removeContract',
+        `Attempting to remove non-existing contract for: ${chainId} ${address}`
+      )
+      return
+    }
+    delete this._contracts[chainId]![address]
+  }
+
+  reset(): void {
+    this._contracts = {}
+  }
+
+  // Returns contract or null
+  getContract<T extends Contract>(chainId: ChainId, address: Address): T | null {
+    return (this._contracts[chainId]?.[address] as T | undefined) ?? null
+  }
+
+  getOrCreateContract<T extends Contract = Contract>(
+    chainId: ChainId,
+    address: Address,
+    provider: providers.Provider,
+    ABI: ContractInterface
+  ): T {
+    const cachedContract = this.getContract<T>(chainId, address)
+    return (cachedContract ?? this.createContract(chainId, address, provider, ABI)) as T
+  }
+}
