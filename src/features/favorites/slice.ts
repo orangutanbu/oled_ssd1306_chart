9a7487c652c486fa53ca752434b5abb437e294b7
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-10 05:38:27 +0000

    crypto: sparc - initialize blkcipher.ivsize

--- src/features/favorites/slice.ts
+++ src/features/favorites/slice.ts
@@ -30,3 +30,63 @@ export const slice = createSlice({
     addFavoriteToken: (
       state,
       { payload: { currencyId } }: PayloadAction<{ currencyId: string }>
+    ) => {
+      if (state.tokens.indexOf(currencyId) === -1) {
+        state.tokens.push(currencyId.toLowerCase()) // normalize all IDs
+      } else {
+        logger.warn(
+          'slice',
+          'addFavoriteToken',
+          `Attempting to favorite a token twice (${currencyId})`
+        )
+      }
+    },
+    removeFavoriteToken: (
+      state,
+      { payload: { currencyId } }: PayloadAction<{ currencyId: string }>
+    ) => {
+      const newTokens = state.tokens.filter((c) => {
+        return c.toLocaleLowerCase() !== currencyId.toLocaleLowerCase()
+      })
+
+      if (newTokens.length === state.tokens.length) {
+        logger.warn(
+          'slice',
+          'removeFavoriteToken',
+          `Attempting to un-favorite a token that was not in favorites (${currencyId})`
+        )
+      }
+
+      state.tokens = newTokens
+    },
+    addWatchedAddress: (state, { payload: { address } }: PayloadAction<{ address: Address }>) => {
+      if (!state.watchedAddresses.includes(address)) {
+        state.watchedAddresses.push(address)
+      } else {
+        logger.warn(
+          'slice',
+          'addWatchedAddress',
+          `Attempting to watch an address twice (${address})`
+        )
+      }
+    },
+    removeWatchedAddress: (
+      state,
+      { payload: { address } }: PayloadAction<{ address: Address }>
+    ) => {
+      const newWatched = state.watchedAddresses.filter((a) => a !== address)
+      if (newWatched.length === state.watchedAddresses.length) {
+        logger.warn(
+          'slice',
+          'removeWatchedAddress',
+          `Attempting to remove an address not found in watched list (${address})`
+        )
+      }
+      state.watchedAddresses = newWatched
+    },
+  },
+})
+
+export const { addFavoriteToken, removeFavoriteToken, addWatchedAddress, removeWatchedAddress } =
+  slice.actions
+export const { reducer: favoritesReducer } = slice
