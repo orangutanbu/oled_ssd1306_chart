06368ff088e53e71fe927e41a036d6329a25d437
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-11 13:40:08 +0000

    Update connect.php

--- src/features/firebase/initFirebaseSaga.ts
+++ src/features/firebase/initFirebaseSaga.ts
@@ -6,4 +6,25 @@ import { logger } from 'src/utils/logger'
 import { call } from 'typed-redux-saga'
 
 export function* initFirebase(): Generator<CallEffect<void>, void, unknown> {
-  yield* call(anonFir
+  yield* call(anonFirebaseSignIn)
+  logger.debug('initFirebaseSaga', 'initFirebase', 'Firebase initialization successful')
+}
+
+function* anonFirebaseSignIn(): Generator<
+  CallEffect<FirebaseAuthTypes.UserCredential>,
+  void,
+  unknown
+> {
+  try {
+    const firebaseAuth = firebase.app().auth()
+    yield* call([firebaseAuth, 'signInAnonymously'])
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    logger.error(
+      'initFirebaseSaga',
+      'anonFirebaseSignIn',
+      `Error signing into Firebase anonymously: ${error?.message}`
+    )
+    throw new Error('Was not able to initialize Firebase user')
+  }
+}
---
+++ src/features/forceUpgrade/README.md
@@ -0,0 +1,4 @@
+# Force Upgrade
+
+The ability of having users force upgrading our app allows us to stop critical UX and security bugs from reaching a wide audience.
+By defining a mininum version all users need to be in, this component will block usage of the app and redirect users to the app st
