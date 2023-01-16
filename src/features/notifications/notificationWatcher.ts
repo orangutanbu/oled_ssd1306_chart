b81b1fef589d7d62ef2c28c9076d9b9a32dec5f7
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-16 13:49:07 +0000

    Renaming files.

--- src/features/notifications/notificationWatcher.ts
+++ src/features/notifications/notificationWatcher.ts
@@ -39,4 +39,91 @@ export function* pushTransactionNotification(
 
   if (typeInfo.type === TransactionType.Approve) {
     const shouldSuppressNotification = yield* call(
-   
+      suppressApproveNotification,
+      from,
+      chainId,
+      addedTime
+    )
+    if (!shouldSuppressNotification) {
+      yield* put(
+        pushNotification({
+          ...baseNotificationData,
+          type: AppNotificationType.Transaction,
+          txType: TransactionType.Approve,
+          tokenAddress: typeInfo.tokenAddress,
+          spender: typeInfo.spender,
+        })
+      )
+    }
+  } else if (typeInfo.type === TransactionType.Swap) {
+    const inputCurrencyAmountRaw = getInputAmountFromTrade(typeInfo)
+    const outputCurrencyAmountRaw = getOutputAmountFromTrade(typeInfo)
+    yield* put(
+      pushNotification({
+        ...baseNotificationData,
+        type: AppNotificationType.Transaction,
+        txType: TransactionType.Swap,
+        inputCurrencyId: typeInfo.inputCurrencyId,
+        outputCurrencyId: typeInfo.outputCurrencyId,
+        inputCurrencyAmountRaw,
+        outputCurrencyAmountRaw,
+        tradeType: typeInfo.tradeType,
+      })
+    )
+  } else if (typeInfo.type === TransactionType.Wrap) {
+    yield* put(
+      pushNotification({
+        ...baseNotificationData,
+        type: AppNotificationType.Transaction,
+        txType: TransactionType.Wrap,
+        currencyAmountRaw: typeInfo.currencyAmountRaw,
+        unwrapped: typeInfo.unwrapped,
+      })
+    )
+  } else if (typeInfo.type === TransactionType.Send) {
+    if (typeInfo?.assetType === AssetType.Currency && typeInfo?.currencyAmountRaw) {
+      yield* put(
+        pushNotification({
+          ...baseNotificationData,
+          type: AppNotificationType.Transaction,
+          txType: TransactionType.Send,
+          assetType: typeInfo.assetType,
+          tokenAddress: typeInfo.tokenAddress,
+          currencyAmountRaw: typeInfo.currencyAmountRaw,
+          recipient: typeInfo.recipient,
+        })
+      )
+    } else if (
+      (typeInfo?.assetType === AssetType.ERC1155 || typeInfo?.assetType === AssetType.ERC721) &&
+      typeInfo?.tokenId
+    ) {
+      yield* put(
+        pushNotification({
+          ...baseNotificationData,
+          type: AppNotificationType.Transaction,
+          txType: TransactionType.Send,
+          assetType: typeInfo.assetType,
+          tokenAddress: typeInfo.tokenAddress,
+          tokenId: typeInfo.tokenId,
+          recipient: typeInfo.recipient,
+        })
+      )
+    }
+  } else if (typeInfo.type === TransactionType.Receive) {
+    const receiveNotification = buildReceiveNotification(action.payload, from)
+    if (receiveNotification) {
+      yield* put(pushNotification(receiveNotification))
+    }
+  } else if (typeInfo.type === TransactionType.WCConfirm) {
+    yield* put(
+      pushNotification({
+        type: AppNotificationType.WalletConnect,
+        event: WalletConnectEvent.TransactionConfirmed,
+        dappName: typeInfo.dapp.name,
+        imageUrl: typeInfo.dapp.icon,
+        chainId: typeInfo.dapp.chain_id,
+      })
+    )
+  } else if (typeInfo.type === TransactionType.Unknown) {
+    yield* put(
+ 
