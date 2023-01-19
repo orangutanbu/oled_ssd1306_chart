39ebf5be9ef3f4f7f65fcc11a3d88129c84e7fdc
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-19 10:37:19 +0000

    Cleanup and refactoring

---
+++ src/features/remoteConfig/testConfigs.ts
@@ -0,0 +1,8 @@
+type ConfigValue = 'disabled' | 'enabled'
+
+/**
+ * List of known test configs.
+ *
+ * To add a read-only remote test config, first create it
+ * in the Firebase Console
+ * @see {@link https://firebase.g
