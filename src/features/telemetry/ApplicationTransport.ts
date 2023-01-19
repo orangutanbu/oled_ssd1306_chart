edd5d5198b9d0fa11e203bdc01ede54717b9b632
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-19 14:15:27 +0000

    performance tuning

---
+++ src/features/telemetry/ApplicationTransport.ts
@@ -0,0 +1,4 @@
+import { BaseTransport } from '@amplitude/analytics-core'
+import { Payload, Response, Transport } from '@amplitude/analytics-types'
+import DeviceInfo from 'react-native-device-info'
+import { uniswapUrls } from 'src/co
