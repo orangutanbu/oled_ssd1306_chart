69fc43e65456406f952a67876d2e5af1720876c6
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-01-31 06:25:41 +0000

    drm/udl: limit modes to the sku pixel limits.

--- src/features/telemetry/hooks.ts
+++ src/features/telemetry/hooks.ts
@@ -1,1 +1,7 @@
-import { useContext, useMemo } from 'r
+import { useContext, useMemo } from 'react'
+import { ITraceContext, TraceContext } from 'src/components/telemetry/Trace'
+
+export function useTrace(trace?: ITraceContext): ITraceContext {
+  const parentTrace = useContext(TraceContext)
+  return useMemo(() => ({ ...parentTrace, ...trace }), [parentTrace, trace])
+}
