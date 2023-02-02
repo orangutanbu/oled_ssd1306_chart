382de29891b49f06292ca6c9fe24e5bc84d1f804
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-02 11:43:38 +0000

    Update project structure.

--- src/features/transactions/SummaryCards/TransactionSummaryLayout.tsx
+++ src/features/transactions/SummaryCards/TransactionSummaryLayout.tsx
@@ -8,4 +8,96 @@ import { BaseButtonProps, TouchableArea } from 'src/components/buttons/Touchable
 import { Flex } from 'src/components/layout/Flex'
 import { SpinningLoader } from 'src/components/loading/SpinningLoader'
 import { BottomSheetModal } from 'src/components/modals/BottomSheetModal'
-import { 
+import { InlineNetworkPill } from 'src/components/Network/NetworkPill'
+import { Text } from 'src/components/Text'
+import { ChainId } from 'src/constants/chains'
+import { ModalName } from 'src/features/telemetry/constants'
+import { useLowestPendingNonce } from 'src/features/transactions/hooks'
+import { cancelTransaction } from 'src/features/transactions/slice'
+import AlertBanner from 'src/features/transactions/SummaryCards/AlertBanner'
+import { CancelConfirmationView } from 'src/features/transactions/SummaryCards/CancelConfirmationView'
+import TransactionActionsModal from 'src/features/transactions/SummaryCards/TransactionActionsModal'
+import {
+  TransactionDetails,
+  TransactionStatus,
+  TransactionType,
+} from 'src/features/transactions/types'
+import { iconSizes } from 'src/styles/sizing'
+import { openMoonpayTransactionLink, openTransactionLink } from 'src/utils/linking'
+
+export const TXN_HISTORY_ICON_SIZE = iconSizes.icon36
+const LOADING_SPINNER_SIZE = 20
+
+function TransactionSummaryLayout({
+  transaction,
+  title,
+  caption,
+  endAdornment,
+  icon,
+  readonly,
+  bg,
+  ...rest
+}: {
+  transaction: TransactionDetails
+  title: string
+  caption: string | undefined
+  readonly: boolean
+  endAdornment?: JSX.Element
+  icon?: JSX.Element
+} & BaseButtonProps): JSX.Element {
+  const { t } = useTranslation()
+
+  const [showActionsModal, setShowActionsModal] = useState(false)
+  const [showCancelModal, setShowCancelModal] = useState(false)
+  const dispatch = useAppDispatch()
+
+  const { status, addedTime, hash, chainId } = transaction
+
+  const showAlertBanner =
+    status === TransactionStatus.Cancelled ||
+    status === TransactionStatus.Cancelling ||
+    status === TransactionStatus.FailedCancel
+  const inProgress = status === TransactionStatus.Cancelling || status === TransactionStatus.Pending
+  const showBackground = showAlertBanner || inProgress
+
+  // Monitor latest nonce to identify queued transactions.
+  const lowestPendingNonce = useLowestPendingNonce()
+  const nonce = transaction?.options?.request?.nonce
+  const queued = nonce && lowestPendingNonce ? nonce > lowestPendingNonce : false
+
+  const isCancelable =
+    status === TransactionStatus.Pending &&
+    !readonly &&
+    Object.keys(transaction.options?.request).length > 0
+
+  function handleCancel(txRequest: providers.TransactionRequest): void {
+    if (!transaction) return
+    dispatch(
+      cancelTransaction({
+        chainId: transaction.chainId,
+        id: transaction.id,
+        address: transaction.from,
+        cancelRequest: txRequest,
+      })
+    )
+    setShowCancelModal(false)
+  }
+
+  // Hide cancelation modal if transaction is no longer pending.
+  useEffect(() => {
+    if (status !== TransactionStatus.Pending) {
+      setShowCancelModal(false)
+    }
+  }, [status])
+
+  const onPress = (): void => {
+    if (readonly) {
+      openTransactionLink(hash, chainId)
+    } else {
+      setShowActionsModal(true)
+    }
+  }
+
+  return (
+    <>
+      <TouchableArea o
