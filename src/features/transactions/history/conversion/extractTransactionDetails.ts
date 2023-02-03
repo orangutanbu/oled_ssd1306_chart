6e4b7336b77989377a27b0a562fcdf8cd8cbe9b4
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-03 16:14:18 +0000

    2003-09-04\tJoel Sherrill <joel@OARcorp.com>

--- src/features/transactions/history/conversion/extractTransactionDetails.ts
+++ src/features/transactions/history/conversion/extractTransactionDetails.ts
@@ -20,4 +20,47 @@ import {
  * @param transaction Transaction api response item to parse.
  * @returns Formatted TransactionDetails object.
  */
-export defau
+export default function extractTransactionDetails(
+  transaction: TransactionListQueryResponse
+): TransactionDetails | null {
+  if (!transaction) return null
+
+  let typeInfo: TransactionTypeInfo | undefined
+  switch (transaction.type) {
+    case ActivityType.Approve:
+      typeInfo = parseApproveTransaction(transaction)
+      break
+    case ActivityType.Send:
+      typeInfo = parseSendTransaction(transaction)
+      break
+    case ActivityType.Receive:
+      typeInfo = parseReceiveTransaction(transaction)
+      break
+    case ActivityType.Swap:
+      typeInfo = parseTradeTransaction(transaction)
+      break
+    case ActivityType.Mint:
+      typeInfo = parseNFTMintTransaction(transaction)
+      break
+  }
+
+  // No match found, default to unknown.
+  if (!typeInfo) {
+    typeInfo = {
+      type: TransactionType.Unknown,
+      tokenAddress: transaction.transaction.to,
+    }
+  }
+
+  return {
+    id: transaction.transaction.hash,
+    // @TODO: [MOB-3901] update with chainId from txn when backend supports other networks
+    chainId: ChainId.Mainnet,
+    hash: transaction.transaction.hash,
+    addedTime: transaction.timestamp * 1000, // convert to ms
+    status: TransactionStatus.Success,
+    from: transaction.transaction.from,
+    typeInfo,
+    options: { request: {} }, // Empty request is okay, gate re-submissions on txn type and status.
+  }
+}
---
+++ src/features/transactions/hooks.ts
@@ -0,0 +1,4 @@
+import { Currency } from '@uniswap/sdk-core'
+import { BigNumberish } from 'ethers'
+import { useCallback, useMemo, useState } from 'react'
+import { LayoutChangeE
