dc97f80eee88d0b457cb82fc4fdd9fa98341519b
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-03 22:19:35 +0000

    Run scan-local.sh on oraclelinux:...

--- src/features/transactions/hooks.ts
+++ src/features/transactions/hooks.ts
@@ -1,4 +1,82 @@
 import { Currency } from '@uniswap/sdk-core'
 import { BigNumberish } from 'ethers'
 import { useCallback, useMemo, useState } from 'react'
-import { LayoutChangeE
+import { LayoutChangeEvent } from 'react-native'
+import { useAppSelector } from 'src/app/hooks'
+import { ChainId } from 'src/constants/chains'
+import { EMPTY_ARRAY } from 'src/constants/misc'
+import { useCurrencyInfo } from 'src/features/tokens/useCurrencyInfo'
+import {
+  makeSelectAddressTransactions,
+  makeSelectTransaction,
+} from 'src/features/transactions/selectors'
+import {
+  createSwapFromStateFromDetails,
+  createWrapFormStateFromDetails,
+} from 'src/features/transactions/swap/createSwapFromStateFromDetails'
+import { TransactionState } from 'src/features/transactions/transactionState/transactionState'
+import {
+  TransactionDetails,
+  TransactionStatus,
+  TransactionType,
+} from 'src/features/transactions/types'
+import { useActiveAccountAddressWithThrow } from 'src/features/wallet/hooks'
+import { theme } from 'src/styles/theme'
+
+// TODO(MOB-3968): Add more specific type definition here
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function usePendingTransactions(
+  address: Address | null,
+  ignoreTransactionTypes = [TransactionType.FiatPurchase]
+) {
+  const transactions = useSelectAddressTransactions(address)
+  return useMemo(() => {
+    if (!transactions) return
+    return transactions.filter(
+      (tx: { status: TransactionStatus; typeInfo: { type: TransactionType } }) =>
+        tx.status === TransactionStatus.Pending &&
+        !ignoreTransactionTypes.includes(tx.typeInfo.type)
+    )
+  }, [ignoreTransactionTypes, transactions])
+}
+
+// sorted oldest to newest
+// TODO(MOB-3968): Add more specific type definition here
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function useSortedPendingTransactions(address: Address | null) {
+  const transactions = usePendingTransactions(address)
+  return useMemo(() => {
+    if (!transactions) return
+    return transactions.sort(
+      (a: TransactionDetails, b: TransactionDetails) => a.addedTime - b.addedTime
+    )
+  }, [transactions])
+}
+
+export function useSelectTransaction(
+  address: Address | undefined,
+  chainId: ChainId | undefined,
+  txId: string | undefined
+): TransactionDetails | undefined {
+  return useAppSelector(makeSelectTransaction(address, chainId, txId))
+}
+
+export function useSelectAddressTransactions(address: Address | null): TransactionDetails[] {
+  return useAppSelector(makeSelectAddressTransactions(address))
+}
+
+export function useCreateSwapFormState(
+  address: Address | undefined,
+  chainId: ChainId | undefined,
+  txId: string | undefined
+): TransactionState | undefined {
+  const transaction = useSelectTransaction(address, chainId, txId)
+
+  const inputCurrencyId =
+    transaction?.typeInfo.type === TransactionType.Swap
+      ? transaction.typeInfo.inputCurrencyId
+      : undefined
+
+  const outputCurrencyId =
+    transaction?.typeInfo.type === TransactionType.Swap
+      ? trans
