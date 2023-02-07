d8b7d1b776c5235dff4c14cf299b7ec32488ce57
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-07 07:06:02 +0000

    micro optimization

--- src/features/transactions/replaceTransaction.ts
+++ src/features/transactions/replaceTransaction.ts
@@ -23,4 +23,91 @@ export function* attemptReplaceTransaction(
   | SelectEffect
   | CallEffect<providers.JsonRpcProvider>
   | CallEffect<SignerManager>
-  | Cal
+  | CallEffect<{
+      transactionResponse: providers.TransactionResponse
+      populatedRequest: providers.TransactionRequest
+    }>
+  | PutEffect<{
+      payload: TransactionDetails
+      type: string
+    }>
+  | PutEffect<{
+      payload: AppNotification
+      type: string
+    }>,
+  void,
+  unknown
+> {
+  const { chainId, hash, options } = transaction
+  logger.debug('replaceTransaction', '', 'Attempting tx replacement', hash)
+  try {
+    const { from, nonce } = options.request
+    if (!from || !nonce || !BigNumber.from(nonce).gte(0)) {
+      throw new Error(`Cannot replace invalid transaction: ${hash}`)
+    }
+
+    const accounts = yield* appSelect(selectAccounts)
+    const account = accounts[getChecksumAddress(from)]
+    if (!account) {
+      throw new Error(`Cannot replace transaction, account missing: ${hash}`)
+    }
+
+    const request: providers.TransactionRequest = {
+      ...newTxRequest,
+      from,
+      nonce,
+    }
+
+    const provider = yield* call(getProvider, chainId)
+    const signerManager = yield* call(getSignerManager)
+
+    const { transactionResponse, populatedRequest } = yield* call(
+      signAndSendTransaction,
+      request,
+      account,
+      provider,
+      signerManager
+    )
+    logger.debug('replaceTransaction', '', 'Tx submitted. New hash:', transactionResponse.hash)
+
+    const updatedTransaction: TransactionDetails = {
+      ...transaction,
+      hash: transactionResponse.hash,
+      status: isCancellation ? TransactionStatus.Cancelling : TransactionStatus.Pending,
+      receipt: undefined,
+      options: {
+        ...options,
+        request: getSerializableTransactionRequest(populatedRequest, chainId),
+      },
+    }
+    yield* put(updateTransaction(updatedTransaction))
+  } catch (error) {
+    logger.error('replaceTransaction', '', 'Error while attempting tx replacement', hash, error)
+
+    // Caught an invalid replacement, which is a failed cancelation attempt. Aka previous
+    // txn was already mined. Mark as finalized.
+    if (transaction.status === TransactionStatus.Cancelling && isCancellation) {
+      const updatedTransaction: TransactionDetails = {
+        ...transaction,
+        hash,
+        status: TransactionStatus.FailedCancel,
+        receipt: undefined,
+        options: {
+          ...options,
+        },
+      }
+      yield* put(
+        finalizeTransaction({ ...updatedTransaction, status: TransactionStatus.FailedCancel })
+      )
+    } else {
+      // Finalize and end attempts to replace.
+      // TODO: [MOB-3898] Can we check for specific errors here?.  Sometimes this might mark actually succesful result.
+      // TODO: [MOB-3898] should we even finalize this?
+      yield* put(finalizeTransaction({ ...transaction, status: TransactionStatus.Success }))
+    }
+
+    yield* put(
+      pushNotification({
+        type: AppNotificationType.Error,
+        address: transaction.from,
+        errorMessage: isCancellatio
