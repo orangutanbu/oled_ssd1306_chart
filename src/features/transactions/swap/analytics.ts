79d3755abcc053677e9984a820f36689cf3add43
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-09 10:45:13 +0000

    Fixed everything

--- src/features/transactions/swap/analytics.ts
+++ src/features/transactions/swap/analytics.ts
@@ -29,4 +29,40 @@ export function useSwapAnalytics(derivedSwapInfo: DerivedSwapInfo): void {
   // run useEffect based on ids since `Currency` objects themselves may be
   // different instances per render
   const inputCurrencyId = inputCurrency && currencyAddress(inputCurrency)
-  const outputCurr
+  const outputCurrencyId = outputCurrency && currencyAddress(outputCurrency)
+
+  // a unique trade is defined by a combination of (input currencyAmount, output token, and trade type)
+  // send analytics event only on unique trades and not on swap quote refreshes
+  useEffect(() => {
+    const currTrade = tradeRef.current
+    if (!currTrade || !inputAmount) return
+
+    sendAnalyticsEvent(
+      SwapEventName.SWAP_QUOTE_RECEIVED,
+      getBaseTradeAnalyticsProperties(currTrade)
+    )
+  }, [inputAmount, inputCurrencyId, outputCurrencyId, tradeType])
+
+  return
+}
+
+export function getBaseTradeAnalyticsProperties(
+  trade: Trade<Currency, Currency, TradeType>
+): SwapTradeBaseProperties {
+  return {
+    token_in_symbol: trade.inputAmount.currency.symbol,
+    token_out_symbol: trade.outputAmount.currency.symbol,
+    token_in_address: getCurrencyAddressForAnalytics(trade.inputAmount.currency),
+    token_out_address: getCurrencyAddressForAnalytics(trade.outputAmount.currency),
+    price_impact_basis_points: trade.priceImpact.multiply(100).toSignificant(),
+    // TODO: [MOB-3904] add gas fee in USD here once we calculate USD value of `totalGasFee` on swap form instead of just on review
+    estimated_network_fee_usd: undefined,
+    chain_id: trade.inputAmount.currency.chainId,
+    token_in_amount: trade.inputAmount.toExact(),
+    token_out_amount: formatCurrencyAmount(trade.outputAmount, NumberType.SwapTradeAmount),
+    allowed_slippage_basis_points:
+      trade.slippageTolerance !== undefined
+        ? BigNumber.from(trade.slippageTolerance).mul(100).toNumber()
+        : undefined,
+  }
+}
