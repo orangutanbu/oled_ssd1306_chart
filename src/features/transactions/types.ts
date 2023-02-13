86cd3f5da9b5a17dc54e21332c62b976d9539c55
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-13 22:35:41 +0000

    more tidying

--- src/features/transactions/types.ts
+++ src/features/transactions/types.ts
@@ -94,4 +94,109 @@ export enum NFTTradeType {
 }
 
 /**
- * Be careful a
+ * Be careful adding to this enum, always assign a unique value (typescript will not prevent duplicate values).
+ * These values are persisted in state and if you change the value it will cause errors
+ */
+export enum TransactionType {
+  // Token Specific
+  Approve = 'approve',
+  Swap = 'swap',
+  Wrap = 'wrap',
+
+  // NFT specific
+  NFTApprove = 'nft-approve',
+  NFTTrade = 'nft-trade',
+  NFTMint = 'nft-mint',
+
+  // All asset types
+  Send = 'send',
+  Receive = 'receive',
+
+  // Fiat onramp
+  FiatPurchase = 'fiat-purchase',
+
+  // General
+  WCConfirm = 'wc-confirm',
+  Unknown = 'unknown',
+}
+
+export interface BaseTransactionInfo {
+  type: TransactionType
+  transactedUSDValue?: number
+}
+
+export interface ApproveTransactionInfo extends BaseTransactionInfo {
+  type: TransactionType.Approve
+  tokenAddress: string
+  spender: string
+  approvalAmount?: string
+}
+
+interface BaseSwapTransactionInfo extends BaseTransactionInfo {
+  type: TransactionType.Swap
+  tradeType: TradeType
+  inputCurrencyId: string
+  outputCurrencyId: string
+}
+
+export interface ExactInputSwapTransactionInfo extends BaseSwapTransactionInfo {
+  tradeType: TradeType.EXACT_INPUT
+  inputCurrencyAmountRaw: string
+  expectedOutputCurrencyAmountRaw: string
+  minimumOutputCurrencyAmountRaw: string
+}
+
+export interface ExactOutputSwapTransactionInfo extends BaseSwapTransactionInfo {
+  tradeType: TradeType.EXACT_OUTPUT
+  outputCurrencyAmountRaw: string
+  expectedInputCurrencyAmountRaw: string
+  maximumInputCurrencyAmountRaw: string
+}
+
+export interface WrapTransactionInfo extends BaseTransactionInfo {
+  type: TransactionType.Wrap
+  unwrapped: boolean
+  currencyAmountRaw: string
+}
+
+export interface SendTokenTransactionInfo extends BaseTransactionInfo {
+  type: TransactionType.Send
+  assetType: AssetType
+  recipient: string
+  tokenAddress: string
+  currencyAmountRaw?: string
+  tokenId?: string // optional. NFT token id
+  nftSummaryInfo?: NFTSummaryInfo // optional. NFT metadata
+}
+
+export interface ReceiveTokenTransactionInfo extends BaseTransactionInfo {
+  type: TransactionType.Receive
+  assetType: AssetType
+  currencyAmountRaw?: string
+  sender: string
+  tokenAddress: string
+  tokenId?: string // optional. NFT token id
+  nftSummaryInfo?: NFTSummaryInfo
+  isSpam?: boolean
+}
+
+export interface FiatPurchaseTransactionInfo extends BaseTransactionInfo {
+  type: TransactionType.FiatPurchase
+  explorerUrl?: string
+  // code will be used for formatting amounts
+  inputCurrency?: Pick<MoonpayCurrency, 'type' | 'code'>
+  inputCurrencyAmount?: number
+  // metadata will be used to get the output currency
+  outputCurrency?: Required<Pick<MoonpayCurrency, 'type' | 'metadata'>>
+  outputCurrencyAmount?: number
+  syncedWithBackend: boolean
+}
+
+export interface NFTMintTransactionInfo extends BaseTransactionInfo {
+  type: TransactionType.NFTMint
+  nftSummaryInfo: NFTSummaryInfo
+  purchaseCurrencyId?: string
+  purchaseCurrencyAmountRaw?: string
+}
+
+export int
