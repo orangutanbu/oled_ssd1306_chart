b0195992ec479f0abd99edd9801219b8d8d5cf38
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-14 14:06:29 +0000

    Add references

--- src/features/walletConnect/api.ts
+++ src/features/walletConnect/api.ts
@@ -5,4 +5,55 @@ const WC_PUSH_SERVER_BASE_URL = 'https://us-central1-uniswap-mobile.cloudfunctio
 const WC_REGISTER_ENDPOINT = 'onWalletConnectRegistration'
 const WC_DEREGISTER_ENDPOINT = 'onWalletConnectDeregistration'
 
-export type RegisterWcPushNotificationParams
+export type RegisterWcPushNotificationParams = {
+  bridge: string
+  topic: string
+  address: string
+  peerName: string
+  language: string
+}
+
+export type DeregisterWcPushNotificationParams = {
+  topic: string
+}
+
+export async function registerWcPushNotifications(
+  params: RegisterWcPushNotificationParams
+): Promise<void> {
+  const request = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(params),
+  }
+
+  try {
+    await fetch(`${WC_PUSH_SERVER_BASE_URL}/${WC_REGISTER_ENDPOINT}`, request)
+  } catch (error) {
+    logger.debug(
+      'walletConnectApi',
+      'registerWcPushNotifications',
+      `Error registering session ${params.topic} for WalletConnect Push Notifications`,
+      error
+    )
+  }
+}
+
+export async function deregisterWcPushNotifications(
+  params: DeregisterWcPushNotificationParams
+): Promise<void> {
+  const request = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(params),
+  }
+  try {
+    await fetch(`${WC_PUSH_SERVER_BASE_URL}/${WC_DEREGISTER_ENDPOINT}`, request)
+  } catch (error) {
+    logger.debug(
+      'walletConnectApi',
+      'deregisterWcPushNotifications',
+      `Error deregistering session ${params.topic} for WalletConnect Push Notifications`,
+      error
+    )
+  }
+}
