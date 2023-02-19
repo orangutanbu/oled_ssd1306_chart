3eb02109e62cd8aeb437e337801d79476ecd461e
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-19 06:01:34 +0000

    Added new test cases

--- src/screens/Import/SeedPhraseInputScreen.tsx
+++ src/screens/Import/SeedPhraseInputScreen.tsx
@@ -7,4 +7,83 @@ import { Button } from 'src/components/buttons/Button'
 import { Box, Flex } from 'src/components/layout'
 import { useLockScreenOnBlur } from 'src/features/authentication/lockScreenContext'
 import { GenericImportForm } from 'src/features/import/GenericImportForm'
-import { importAccountActions, IMPORT_WALLET_AMOUNT } from 'src/feat
+import { importAccountActions, IMPORT_WALLET_AMOUNT } from 'src/features/import/importAccountSaga'
+import { ImportAccountType } from 'src/features/import/types'
+import { SafeKeyboardOnboardingScreen } from 'src/features/onboarding/SafeKeyboardOnboardingScreen'
+import { ElementName } from 'src/features/telemetry/constants'
+import { OnboardingScreens } from 'src/screens/Screens'
+import {
+  MnemonicValidationError,
+  translateMnemonicErrorMessage,
+  userFinishedTypingWord,
+  validateMnemonic,
+  validateSetOfWords,
+} from 'src/utils/mnemonics'
+import { useAddBackButton } from 'src/utils/useAddBackButton'
+
+type Props = NativeStackScreenProps<OnboardingStackParamList, OnboardingScreens.ImportMethod>
+
+export function SeedPhraseInputScreen({ navigation, route: { params } }: Props): JSX.Element {
+  const dispatch = useAppDispatch()
+  const { t } = useTranslation()
+
+  /**
+   * If paste permission modal is open, we need to manually disable the splash screen that appears on blur,
+   * since the modal triggers the same `inactive` app state as does going to app switcher
+   *
+   * Technically seed phrase will be blocked if user pastes from keyboard,
+   * but that is an extreme edge case.
+   **/
+  const [pastePermissionModalOpen, setPastePermissionModalOpen] = useState(false)
+  useLockScreenOnBlur(pastePermissionModalOpen)
+
+  const [value, setValue] = useState<string | undefined>(undefined)
+  const [showSuccess, setShowSuccess] = useState(false)
+  const [errorMessage, setErrorMessage] = useState<string | undefined>(undefined)
+
+  useAddBackButton(navigation)
+
+  // Add all accounts from mnemonic.
+  const onSubmit = useCallback(() => {
+    // Check phrase validation
+    const { validMnemonic, error, invalidWord } = validateMnemonic(value)
+
+    if (error) {
+      setErrorMessage(translateMnemonicErrorMessage(error, invalidWord, t))
+      return
+    }
+
+    dispatch(
+      importAccountActions.trigger({
+        type: ImportAccountType.Mnemonic,
+        validatedMnemonic: validMnemonic,
+        indexes: Array.from(Array(IMPORT_WALLET_AMOUNT).keys()),
+      })
+    )
+    navigation.navigate({ name: OnboardingScreens.SelectWallet, params, merge: true })
+  }, [value, t, dispatch, navigation, params])
+
+  const onBlur = useCallback(() => {
+    const { error, invalidWord } = validateMnemonic(value)
+    if (error) {
+      setShowSuccess(false)
+      setErrorMessage(translateMnemonicErrorMessage(error, invalidWord, t))
+    }
+  }, [t, value])
+
+  const onChange = (text: string | undefined): void => {
+    const { error, invalidWord, isValidLength } = validateSetOfWords(text)
+
+    // always show success UI if phrase is valid length
+    if (isValidLength) {
+      setShowSuccess(true)
+    } else {
+      setShowSuccess(false)
+    }
+
+    // suppress error messages if the  user is not done typing a word
+    const suppressError =
+      (error === MnemonicValidationError.InvalidWord && !userFinishedTypingWord(text)) ||
+      error === MnemonicValidationError.NotEnoughWords
+
+  
