2913b417f512b43ee6078cbf28b3d90b6fd9c13a
orangutanbu <105702384+orangutanbu@users.noreply.github.com>
2023-02-24 07:30:58 +0000

    added 404.html

---
+++ src/styles/sizing.ts
@@ -0,0 +1,30 @@
+import { Dimensions } from 'react-native'
+
+export const dimensions = {
+  fullHeight: Dimensions.get('window').height,
+  fullWidth: Dimensions.get('window').width,
+}
+
+export const spacing = {
+  none: 0,
+  spacing1: 1,
+  spacing2: 2,
+  spacing4: 4,
+  spacing8: 8,
+  spacing12: 12,
+  spacing16: 16,
+  spacing24: 24,
+  spacing36: 36,
+  spacing48: 48,
+  spacing60: 60,
+}
+
+export const iconSizes = {
+  icon8: 8,
+  icon12: 12,
+  icon16: 16,
+  icon20: 20,
+  icon24: 24,
+  icon28: 28,
+  icon36: 36,
+ 
